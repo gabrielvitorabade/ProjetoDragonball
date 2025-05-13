0eecadc02e473189d3fdd84ec2b47799cfef79b3
Gabriel Abade <161393183+gabrielvitorabade@users.noreply.github.com>
2025-05-13 18:18:38 -0400

    feat: implement user registration functionality and update login page styles

--- back/app.js
+++ back/app.js
@@ -1,6 +1,7 @@
-const express = require('express');
+const express = require ('express');
+const validator = require('validator');
 const app = express();
-const {connection} = require('./db_connection.js'); // Import the database connection
+const {connection} = require ('./db_connection.js'); // Import the database connection
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -42,7 +43,42 @@ app.post('/login', (req, res) => {
    }
 });
 
+app.post('/register',(req,res)=>{
+  const {email,password, confirmPassword} = req.body;
+  
+  try{
+    if(!email || !password || !confirmPassword){
+      return res.status(400).json({ message: 'Email and password  are required' });
+    }
+     else{
+      connection.query('SELECT email FROM users WHERE email = ?',[email],(error,results)=>{
+        if (error) {
+          console.error('Error executing query:', error);
+          return res.status(500).json({ message: 'Internal server error' });
+        }
+        if (results.length > 0) {
+          return res.status(400).json({ message: 'Email already registered' });
+        } else if(validator.isEmail(email) === false) {
+          return res.status(400).json({ message: 'Invalid email format' });
+        }else if (password !== confirmPassword){
+             return res.status(400).json({message: 'Password and confirm password must be the same'});
+         }
+        else{connection.query('INSERT INTO users (email, password) VALUES (?, ?)', [email, password], (error, results) => {
+          if (error) {
+            console.error('Error executing query:', error);
+            return res.status(500).json({ message: 'Erro to register user, try again later.' });
+          }
+          return res.status(200).json({ message: 'User registered successfully!' });}
+        );
+      }
+    })
+  }
+}
+catch(error){
+  console.error('Error:', error);
+  res.status(400).json({ message: 'Error to require. Try again later.' });
+}
+})
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
-// This code sets up a simple Express server that listens for POST requests on the /login endpoint.
---
+++ front/script/register.js
@@ -0,0 +1,26 @@
+function doRegister() {
+  const email = document.querySelector('#email').value;
+  const password = document.querySelector('#password').value;
+  const confirmPassword = document.querySelector('#confirm-password').value;
+
+  fetch('http://localhost:3000/register', {
+      method: 'POST',
+      mode: 'cors',
+      body: JSON.stringify({ email, password, confirmPassword }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+  })
+    .then(async (response) => {
+      let responseJSON = await response.json();
+      if (response.ok) {
+        window.location.href = 'login.html';
+      } else {
+        alert(responseJSON.message); // Exibe a mensagem de erro retornada pelo servidor
+      }
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      alert('Falha na requisição, tente novamente mais tarde.'); // Exibe a mensagem de erro
+    });
+}
